0076a76a4c1172ea9ad5b6701830a57ecb53586e
Jian Gong <hello@jgong.io>
2025-04-16 18:01:49 -0700

    Add some logging

--- sync.js
+++ sync.js
@@ -1,12 +1,14 @@
 import Ably from 'https://esm.sh/ably';
 
 export const setup = () => {
+  console.log('Setting up sync');
   const clientId = 'client_' + Math.random().toString(36).substring(2, 15);
   const ably = new Ably.Realtime("codXNw.EPiBTw:Fu3NmW5QV5OcyOQj7lMQlXITNPpZbJqG6GP8pcnIqno");
 
   const channel = ably.channels.get("proto-chan");
 
   channel.subscribe("event", (message) => {
+    console.log('Received sync message:', message.data);
     if (message.data.clientId === clientId) {
       return;
     }
@@ -23,6 +25,8 @@ export const setup = () => {
     // Dispatch the event on the window object
     window.dispatchEvent(syncEvent);
   });
+
+  console.log('Setup complete');
   
   return {
     clientId,
